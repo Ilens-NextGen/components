b15ffcbf578d25dc665ec242c5c9934c18f01eaa
Ugwuanyi Afam <111662034+2022phyro@users.noreply.github.com>
2024-01-09 21:04:58 +0000

    Fixed it to run  in a wsgi server. Now to attempt to make it run in an asgi server

--- user_management/core/wsgi.py
+++ user_management/core/wsgi.py
@@ -4,6 +4,8 @@ from django.core.wsgi import get_wsgi_application
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 from streamer.views import sio
+import logging
+logging.basicConfig(level=logging.INFO)  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
@@ -15,6 +17,6 @@ sio_app = socketio.WSGIApp(sio, django_app)
 
 # Combine Django and Socket.IO
 application = pywsgi.WSGIServer(("", 8000), sio_app, handler_class=WebSocketHandler)
-
 # Uncomment the line below if you want to serve the application using gevent's server
 application.serve_forever()
+application.log = logging.get_logger()
